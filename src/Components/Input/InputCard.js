d0742a749aca1032467bd1a053af05accbe51610
mostafa-ali-a <mlovelylord@gmail.com>
2022-12-20 18:28:40 +0200

    Configure for deployment to GitHub Pages

--- src/Components/Input/InputCard.js
+++ src/Components/Input/InputCard.js
@@ -33,11 +33,9 @@ function InputCard({ setOpen, listId }) {
         onClick= {() => {setOpen(false);addMoreCard(text, listId);setText('')}}
         >Add card</Button>
         <ClearIcon
-        fontSize='large'
         className= 'inputCard clear'
         onClick= {() => setOpen(false)} />
         <MoreHoriz
-        fontSize='large'
         className= 'horiz icon' />
       </div>
     </div>
