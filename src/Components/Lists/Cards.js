b1be3c063f8e0e10d39c5ab5cd83f5f832c0b81a
mostafa-ali-a <mlovelylord@gmail.com>
2022-12-26 16:38:43 +0200

    Configure for deployment to GitHub Pages

--- src/Components/Lists/Cards.js
+++ src/Components/Lists/Cards.js
@@ -23,7 +23,6 @@ function Cards({ card }) {
               multiline
               autoFocus
               fullWidth
-
               type={'text'}
               />
               <Button className= 'card btn button'
